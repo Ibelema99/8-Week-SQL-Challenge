5d50bba9471bead388a5ebc294e77625af46980c
Ibelema99 <110858866+Ibelema99@users.noreply.github.com>
2025-04-07 21:32:02 -0400

    Add files via upload

---
+++ Case Study 2 - Pizza Runner/Pizza Runner 1 - tables and cleaning.sql
@@ -0,0 +1,108 @@
+create schema pizza_runner;
+
+-- create table 1: runners table; 
+create table runners (runner_id int, registration_date date);
+insert into runners(runner_id, registration_date) 
+values (1, '2021-01-01'),
+		(2, '2021-01-03'), 
+        (3,	'2021-01-08'),
+        (4, '2021-01-15');
+        
+
+-- create table 2: customer_orders table;
+create table customer_orders (order_id int, customer_id int, pizza_id int, 
+						exclusions varchar(4), extras varchar(4), ordertime datetime);
+insert into customer_orders (order_id, customer_id, pizza_id, exclusions, extras, ordertime)
+values ('1', '101', '1', '', '', '2020-01-01 18:05:02'),
+	  ('2', '101', '1', '', '', '2020-01-01 19:00:52'),
+	  ('3', '102', '1', '', '', '2020-01-02 23:51:23'),
+	  ('3', '102', '2', '', NULL, '2020-01-02 23:51:23'),
+	  ('4', '103', '1', '4', '', '2020-01-04 13:23:46'),
+	  ('4', '103', '1', '4', '', '2020-01-04 13:23:46'),
+	  ('4', '103', '2', '4', '', '2020-01-04 13:23:46'),
+	  ('5', '104', '1', 'null', '1', '2020-01-08 21:00:29'),
+	  ('6', '101', '2', 'null', 'null', '2020-01-08 21:03:13'),
+	  ('7', '105', '2', 'null', '1', '2020-01-08 21:20:29'),
+	  ('8', '102', '1', 'null', 'null', '2020-01-09 23:54:33'),
+	  ('9', '103', '1', '4', '1, 5', '2020-01-10 11:22:59'),
+	  ('10', '104', '1', 'null', 'null', '2020-01-11 18:34:49'),
+	  ('10', '104', '1', '2, 6', '1, 4', '2020-01-11 18:34:49');
+
+-- create table 3: runner_orders table;
+create table runner_orders (order_id INT, runner_id INT, pickup_time VARCHAR(19),
+				distance VARCHAR(7), duration VARCHAR(10), cancellation VARCHAR(23));
+insert into runner_orders (order_id, runner_id, pickup_time, distance, duration, cancellation)
+values ('1', '1', '2020-01-01 18:15:34', '20km', '32 minutes', ''),
+	  ('2', '1', '2020-01-01 19:10:54', '20km', '27 minutes', ''),
+	  ('3', '1', '2020-01-03 00:12:37', '13.4km', '20 mins', NULL),
+	  ('4', '2', '2020-01-04 13:53:03', '23.4', '40', NULL),
+	  ('5', '3', '2020-01-08 21:10:57', '10', '15', NULL),
+	  ('6', '3', 'null', 'null', 'null', 'Restaurant Cancellation'),
+	  ('7', '2', '2020-01-08 21:30:45', '25km', '25mins', 'null'),
+	  ('8', '2', '2020-01-10 00:15:02', '23.4 km', '15 minute', 'null'),
+	  ('9', '2', 'null', 'null', 'null', 'Customer Cancellation'),
+	  ('10', '1', '2020-01-11 18:50:20', '10km', '10minutes', 'null');
+        
+
+-- create table 4: pizza_names table;
+create table pizza_names (pizza_id int, pizza_name varchar(20));
+insert into pizza_names(pizza_id, pizza_name)
+values (1, 'Meat Lovers'),
+		(2,	'Vegetarian');
+
+
+-- create table 5: pizza_recipes table;
+create table pizza_recipes (pizza_id int, toppings varchar(30));
+insert into pizza_recipes(pizza_id, toppings)
+values (1, '1, 2, 3, 4, 5, 6, 8, 10'),
+		(2,	'4, 6, 7, 9, 11, 12');
+
+-- create table 6: pizza_toppings table;
+create table pizza_toppings (topping_id int, topping_name varchar(30));
+insert into pizza_toppings(topping_id, topping_name)
+values (1, 'Bacon'),
+		(2, 'BBQ Sauce'),
+		(3, 'Beef'),
+		(4, 'Cheese'),
+		(5,	'Chicken'),
+		(6,	'Mushrooms'),
+		(7,	'Onions'),
+		(8,	'Pepperoni'),
+		(9,	'Peppers'),
+		(10, 'Salami'),
+		(11, 'Tomatoes'),
+		(12, 'Tomato Sauce');
+
+-- Cleaning the data
+-- 1. make all empty and null values in the customer_orders table null 
+create table temp_customer_orders as select order_id, customer_id, pizza_id,
+case when exclusions = '' or exclusions like 'null' then null else exclusions end as exclusions, 
+case when extras = '' or extras like 'null' then null else extras end as extras, ordertime
+from customer_orders;
+
+
+/*2. In runner_orders, replace 'null' by null, remove km from distance and minutes from duration 
+- change duration to int distance to float*/
+
+create table temp_runner_orders as select order_id, runner_id,
+case when pickup_time like 'null' then null else pickup_time end as pickup_time, 
+case when distance like 'null' then null
+	when distance like '%km' then trim('km' from distance) else distance end as distance,
+case when duration like 'null' then null 
+	when duration like '%mins' then trim('mins' from duration)
+    when duration like '%minute' then trim('minute' from duration)
+    when duration like '%minutes' then trim('minutes' from duration) else duration end as duration,
+case when cancellation like 'null' or cancellation = '' then null else cancellation end as cancellation
+from runner_orders; 
+
+-- 3. change datatypes for temp_runner_orders
+alter table temp_runner_orders
+modify column pickup_time datetime,
+modify column distance float,
+modify column duration int;
+
+
+
+select * from temp_customer_orders;
+select * from temp_runner_orders;
+ 
